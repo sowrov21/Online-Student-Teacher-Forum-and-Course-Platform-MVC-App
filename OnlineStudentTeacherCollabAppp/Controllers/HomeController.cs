0b243cc56f0b8f64afbfcf3c6327f03c0b02e420
Sowrov21 <sowrov.it@gmail.com>
2021-07-05 19:38:31 +0600

    Create All User Login by Type and Layout

--- OnlineStudentTeacherCollabAppp/Controllers/HomeController.cs
+++ OnlineStudentTeacherCollabAppp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace OnlineStudentTeacherCollabAppp.Controllers
     {
         public ActionResult Index()
         {
-            //
+         
             return View();
         }
 
---
+++ OnlineStudentTeacherCollabAppp/Controllers/Student/StudentDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStudentTeacherCollabAppp.Controllers.Student
+{
+    public class StudentDashboardController : Controller
+    {
+        // GET: StudentDashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
